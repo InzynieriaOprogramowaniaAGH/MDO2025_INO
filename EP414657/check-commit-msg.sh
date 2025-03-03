3af0f3d4199cc64fcc72aaf53bd170157938b17f
Emilia Pajdo <pajdoemilia@gmail.com>
2025-03-03 20:10:38 +0100

    EP414657 Dodanie pliku sprawozdania i githook

---
+++ EP414657/check-commit-msg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+commit_msg_file="$1"
+commit_msg=$(cat "$commit_msg_file")
+
+if [[$commit_msg =~ ^EP414657 ]]; then 
+ echo "ERROR: Commit message must start with 'EP414657' "
+ exit 1
+fi
