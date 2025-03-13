c94ebf5a06d338c74ed7a530b906a5c86097effb
Emilia Pajdo <pajdoemilia@gmail.com>
2025-03-13 12:23:46 +0100

    EP414657 Usunięto pliki

--- EP414657/check-commit-msg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-commit_msg_file="$1"
-commit_msg=$(cat "$commit_msg_file")
-
-if [[$commit_msg =~ ^EP414657 ]]; then 
- echo "ERROR: Commit message must start with 'EP414657' "
- exit 1
-fi
