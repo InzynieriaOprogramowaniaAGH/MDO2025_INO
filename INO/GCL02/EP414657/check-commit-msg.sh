8858cb15459c5562368b40db2ab115edeb259f18
Emilia Pajdo <pajdoemilia@gmail.com>
2025-03-13 16:10:30 +0100

    Zaktualizowano pliki

--- INO/GCL02/EP414657/check-commit-msg.sh
+++ INO/GCL02/EP414657/check-commit-msg.sh
@@ -2,7 +2,7 @@
 commit_msg_file="$1"
 commit_msg=$(cat "$commit_msg_file")
 
-if [[$commit_msg =~ ^EP414657 ]]; then 
+if [[ $commit_msg =~ ^EP414657 ]]; then 
  echo "ERROR: Commit message must start with 'EP414657' "
  exit 1
 fi
