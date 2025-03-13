7b2b10ac4b2cf46d8c820d7d147a1b0fbda08fab
ALOK1101 <aleksander.rutkowski11@gmail.com>
2025-03-13 16:01:38 +0100

    AR417143 sprawozdanie z hookiem

---
+++ ITE/GCL06/AR417143/commit-msg-hook.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+commit_msg=$(cat "$1") 
+if ! [[ $commit_msg =~ ^KD232144 ]]; then 
+echo "Error: Commit messege musi zaczynac sie od 'AR417143'" 
+echo "Commit messege: $commit_msg"
+exit 1 
+fi 
+exit 0
