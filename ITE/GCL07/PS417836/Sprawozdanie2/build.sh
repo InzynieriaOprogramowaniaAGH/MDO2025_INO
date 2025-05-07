e23d2e37fe0b4bb934b70ca7afc2a0a68f9cc34f
Twoje Imię Nazwisko <twoj.email@example.com>
2025-05-07 19:26:52 +0200

    PS417836 lab 5, 6, 7

--- ITE/GCL07/PS417836/Sprawozdanie2/build.sh
+++ ITE/GCL07/PS417836/Sprawozdanie2/build.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 dh_make --single --createorig -c gpl3 --email "bw@example.com" --yes
 
 cat << 'EOF' > debian/rules
 #!/usr/bin/make -f
 
 %:
-    dh \$@ --buildsystem=meson
+	dh $@ --buildsystem=meson
 EOF
 
 chmod +x debian/rules
